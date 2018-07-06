441ea2879b867e72c9bf139cace4718d21e2563f
gff1055 <gff105@gmail.com>
2018-07-06 17:38:24 -0300

    login e senha

--- App/Views/formBusca.php
+++ App/Views/formBusca.php
@@ -1,19 +1,20 @@
 <?php
 use App\Init;
-?>
-
-<form method="Post" action="../public/busca">
-	<input type="text" name="pesquisa"/>
-	<input type="submit" value="Buscar"/>
-	<input type="hidden" name="tipoPesquisa" value="Tudo">
-</form>
-<?php
 
 // mostra o nome do usuario logado
-if (isset($_SESSION['login']))
+if (isset($_SESSION['login'])){
 			echo "Usuário: ".$_SESSION['login'];
-		else
-			echo "Usuário: Não conectado";
+	?>
+	<a href="<?php echo Init::$urlRoot?>/logout">Fazer Logoff</a>
+	<form method="Post" action="../public/busca">
+		<input type="text" name="pesquisa"/>
+		<input type="submit" value="Buscar"/>
+		<input type="hidden" name="tipoPesquisa" value="Tudo">
+	</form>
+	<?php
+}
+else
+	echo "Usuário: Não conectado";
 ?>
 
-<a href="<?php echo Init::$urlRoot?>/logout">Fazer Logoff</a>
+
