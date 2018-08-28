9252be2fb4de5676ba3afe54e0dbede46dcfa498
gff1055 <guilhermeferreirafb@gmail.com>
2018-08-28 20:34:35 -0300

    corrigindo feed de noticias

--- App/Controllers/ControllerIndex.php
+++ App/Controllers/ControllerIndex.php
@@ -32,6 +32,8 @@ class ControllerIndex{
 			$page = "../App/Views/login.php";
 		}
 		$this->cab->abertura($titleBar); // cabecalho da pagina
+		include_once "../App/Views/mostraUsuario.php";
+		include_once "../App/Views/formBusca.php";
 		include_once $page; // conteudo
 		$this->cab->fechamento(); // fechando a pagina
 	}
@@ -55,7 +57,7 @@ class ControllerIndex{
 			$_SESSION['id'] = $isOk['codigo'];
 			echo "<br>SESSION[id] =". $isOk['codigo'];
 
-			header("location: ../public/".$isOk['nick']); // redirecionando para a pagina inicial
+			header("location: ../public/"); // redirecionando para a pagina inicial
 		}
 
 		// no caso de nao existir o usuario e senha digitados
--- App/Views/newsfeed.php
+++ App/Views/newsfeed.php
@@ -1,10 +1,9 @@
 <?php
 
 foreach($arrayNewsFeed as $userNews){
-	echo "<b>".$userNews['name']."</b>";
+	echo "<br><br><br><b>".$userNews['name']."</b>";
 	echo "<br><br>".$userNews['content'];
 	echo "<h6>".$userNews['date']."</h6>";
-	echo "<br><br><br>";
 }
 
 ?>
